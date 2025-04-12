494675ad0bab64c548fbc694b4e348f368b70c62
Dhaneshperumal <dhaneshperumal@gmail.com>
2025-04-12 10:22:20 +0530

    project init

---
+++ medplus/src/components/auth/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+        <div>
+            <h1>Login</h1>
+            <form>
+               
+                <label>Enter Email/Mobile : </label> 
+                <input type="text" name="contact" /> 
+               
+            </form>
+        </div>
+    </>
+  )
+}
+
+export default Login
